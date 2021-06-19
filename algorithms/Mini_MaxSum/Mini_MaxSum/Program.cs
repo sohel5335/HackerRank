9a0724c907df343452767e950c2994e86443e688
ukwldeveloper02@gmail.com <alamin.sohel02>
2021-06-20 01:00:12 +0600

    sohel

--- algorithms/Mini_MaxSum/Mini_MaxSum/Program.cs
+++ algorithms/Mini_MaxSum/Mini_MaxSum/Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace Mini_MaxSum
+class Result
 {
-    class Program
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        var rmin = arr;
+        var rmax = arr;
+        int a= rmin.Min();
+        rmin.Remove(a);
+        long max =rmin.Sum(x=>(long)x);
+        rmax.Remove(rmax.Max());
+
+        long min = rmax.Sum(x => (long)x) +a;
+
+        Console.WriteLine(min + " " + max);
+        //Console.ReadLine();
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
     }
 }
