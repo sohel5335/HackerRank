e77e6953505eadb7e99b04603d1dc8fa024235e0
ukwldeveloper02@gmail.com <alamin.sohel02>
2021-06-26 15:52:22 +0600

    'sohel'

---
+++ algorithms/LuckBalance/LuckBalance/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LuckBalance
+{
+    class Result
+    {
+
+       
+
+        public static int luckBalance(int k, List<List<int>> contests,int len)
+        {
+            int importantC=0,MaxLuck=0;
+            List<int> tmparr =new List<int> ();
+            for (int i=0;i<len;i++)
+            {
+                MaxLuck += contests[i][0];
+                if (contests[i][1] == 1)
+                {
+                    importantC++;
+                    int tmp = contests[i][0];
+                    int tmplen = tmparr.Count;
+                    for (int j=0;j< tmplen;j++)
+                    {
+                        if (tmparr[j]>tmp)
+                        {
+                            int a = tmparr[j];
+                            tmparr[j] = tmp;
+                            tmp = a;
+                        }
+
+                    }
+                    tmparr.Add(tmp);
+                }
+            }
+
+            int lowestWin = importantC - k;
+
+            for (int i=0;i<lowestWin;i++)
+            {
+                MaxLuck -= (tmparr[i]+ tmparr[i]);
+            }
+
+
+            return MaxLuck;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] firstMultiInput = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32( firstMultiInput[0]);
+            int k = Convert.ToInt32(firstMultiInput[1]);
+            List<List<int>> contests = new List<List<int>>();
+            for (int i=0;i<n;i++)
+            {
+                contests.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(x=>Convert.ToInt32(x)).ToList());
+            }
+            int result = Result.luckBalance(k, contests, n);
+
+            Console.WriteLine(result);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ algorithms/MaximumPerimeterTriangle/MaximumPerimeterTriangle/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'maximumPerimeterTriangle' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY sticks as parameter.
+     */
+
+    public static void maximumPerimeterTriangle(List<int> sticks)
+    {
+        List<int> a = new List<int>();
+        a = sticks.OrderBy(u=>u).ToList();
+        //Array.Sort(sticks);
+        int n = sticks.Count();
+        int x, y, z;
+        for (x = n - 3, y = n - 2, z = n - 1; a[x] + a[y] <= a[z]; x--, y--, z--)
+        {
+            if (x == 0)
+            {
+                Console.WriteLine(-1);
+                return;
+            }
+        }
+        Console.WriteLine("{0} {1} {2}", a[x], a[y], a[z]);
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> sticks = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sticksTemp => Convert.ToInt32(sticksTemp)).ToList();
+
+            Result.maximumPerimeterTriangle(sticks);
+
+
+
+            //Console.ReadLine();
+
+            //textWriter.WriteLine(String.Join(" ", result));
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
