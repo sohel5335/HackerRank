55bdbd28d403a0e8c4fab0a92e5399dead563b69
sohel5335 <rsohel5335@gmail.com>
2021-09-04 17:57:34 +0600

    algo

---
+++ algorithms/BetweenTwoSets/BetweenTwoSets/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static int getTotalX(List<int> a, List<int> b)
+    {
+        int f = lcm(a.ToArray());
+        int l = gcd(b.ToArray());
+        int count = 0;
+        for (int i = f, j = 2; i <= l; i = f * j, j++)
+        {
+            if (l % i == 0) { count++; }
+        }
+        return count;
+    }
+    private static int gcd(int a, int b)
+    {
+        while (b > 0)
+        {
+            int temp = b;
+            b = a % b; // % is remainder
+            a = temp;
+        }
+        return a;
+    }   
+    private static int lcm(int a, int b)
+    {
+        return a * (b / gcd(a, b));
+    }
+    private static int gcd(int[] input)
+    {
+        int result = input[0];
+        for (int i = 1; i < input.Count(); i++)
+        {
+            result = gcd(result, input[i]);
+        }
+        return result;
+    }
+
+    private static int lcm(int[] input)
+    {           
+        int result = input[0];
+        for (int i = 1; i < input.Count(); i++)
+        {
+            result = lcm(result, input[i]);
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> brr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+        int total = Result.getTotalX(arr, brr);
+
+        Console.WriteLine(total);
+        //Console.ReadLine();
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
---
+++ algorithms/BreakingtheRecords/BreakingtheRecords/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<int> breakingRecords(List<int> scores)
+    {
+
+        List<int> result = new List<int> { 0,0};
+        int maxScore = 0, minScore = 0;
+
+         maxScore = minScore = scores[0];
+
+       for(int i=1;i< scores.Count; i++)
+        { 
+            if(scores[i]> maxScore)
+            {
+                result[0]++;
+                maxScore = scores[i];
+            }
+            if(scores[i] < minScore)
+            {
+                minScore = scores[i];
+                result[1]++;
+            }
+        }
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+        List<int> result = Result.breakingRecords(scores);
+
+        Console.WriteLine(String.Join(" ", result));
+        //Console.ReadLine();
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
